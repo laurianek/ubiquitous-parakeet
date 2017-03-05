c4a671619d988b2a5c22b653ef12590eb6865ae8
Lauriane Kayungu-N <lauriane.kayungu@gmail.com>
2017-03-05 10:06:45 +0000

    work in progress - getting some behaviour working

--- src/assets/bundle.js
+++ src/assets/bundle.js
@@ -1,3 +1,6 @@
+"use strict";
+
+(function () {})();
 'use strict';
 
 (function () {
@@ -8,11 +11,38 @@
   };
 
   var MAX_SQUARE = 144;
-  var container = document.getElementById('squares-container');
 
-  var innerHtml = '';
-  for (var i = 0; i < MAX_SQUARE; i++) {
-    innerHtml += '<div class="square" id="square_' + (i + 1) + '">' + (i + 1) + '</div>';
+  function init() {
+    var container = document.getElementById('squares-container');
+
+    container.addEventListener('click', function (e) {
+      var el = e.target;
+      var number = Number(el.id.split('square_').join(''));
+      highLightMultiplesOf(number);
+    });
+
+    var innerHtml = '';
+    for (var i = 0; i < MAX_SQUARE; i++) {
+      innerHtml += '<div class="square" id="square_' + (i + 1) + '">' + (i + 1) + '</div>';
+    }
+    container.innerHTML = innerHtml;
   }
-  container.innerHTML = innerHtml;
+
+  function highLightMultiplesOf(number) {
+    document.querySelectorAll('.square').forEach(function (s) {
+      return s.classList.remove('square--highlight');
+    });
+
+    findMultiples(number).forEach(function (m) {
+      document.getElementById('square_' + m).classList.add('square--highlight');
+    });
+  }
+
+  function findMultiples(number) {
+    var multiples = [];
+    for (var i = number; i <= MAX_SQUARE; i += number) {
+      multiples.push(i);
+    }
+    return multiples;
+  }
 })();
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,11 +6,37 @@
   };
 
   const MAX_SQUARE = 144;
-  const container = document.getElementById('squares-container');
 
-  let innerHtml = '';
-  for (let i = 0; i < MAX_SQUARE; i++) {
-    innerHtml += `<div class="square" id="square_${i + 1}">${i + 1}</div>`;
+  function init() {
+    const container = document.getElementById('squares-container');
+
+    container.addEventListener('click', (e) => {
+      const el = e.target;
+      const number = Number(el.id.split('square_').join(''));
+      highLightMultiplesOf(number);
+    });
+
+    let innerHtml = '';
+    for (let i = 0; i < MAX_SQUARE; i++) {
+      innerHtml += `<div class="square" id="square_${i + 1}">${i + 1}</div>`;
+    }
+    container.innerHTML = innerHtml;
   }
-  container.innerHTML = innerHtml;
+
+  function highLightMultiplesOf(number) {
+    document.querySelectorAll('.square')
+      .forEach(s => s.classList.remove('square--highlight'));
+
+    findMultiples(number).forEach(m => {
+      document.getElementById(`square_${m}`).classList.add('square--highlight');
+    });
+  }
+
+  function findMultiples(number) {
+    const multiples = [];
+    for (let i = number; i <= MAX_SQUARE; i += number) {
+      multiples.push(i);
+    }
+    return multiples;
+  }
 })();
